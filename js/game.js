2748cb1ea9b0ce5aecfb42e73bca067e6fec0f5a
Steven Inouye <steveninouye@msn.com>
2018-01-25 19:06:34 -1000

    spaceship is moving

--- js/game.js
+++ js/game.js
@@ -1,3 +1,46 @@
 (Phaser => {
-  console.log(Phaser);
+  const GAME_WIDTH = 460;
+  const GAME_HEIGHT = 600;
+  const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+  const INITIAL_MOVESPEED = 4;
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+  let player;
+  let cursors;
+
+  function preload() {
+    game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
+  };
+
+  function create() {
+    cursors = game.input.keyboard.createCursorKeys();
+    player = game.add.sprite(100, 100, GFX, 8);
+    player.moveSpeed = INITIAL_MOVESPEED;
+  };
+
+  function handlePlayerMovement(){
+    switch(true){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch(true){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
+  }
+
+  function update() {
+    handlePlayerMovement();
+  };
+
 })(window.Phaser);
