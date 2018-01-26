ad2adade33af6ad80f83b9a4ee45896910d79a7f
Steven Inouye <steveninouye@msn.com>
2018-01-25 19:17:36 -1000

    console logging the fire of shot

--- js/game.js
+++ js/game.js
@@ -9,6 +9,40 @@
 
   let player;
   let cursors;
+  let playerBullets;
+
+  function handlePlayerMovement(){
+    let movingH = Math.sqrt(2);
+    let movingV = Math.sqrt(2);
+    if(cursors.up.isDown || cursors.down.isDown){
+      movingH = 1;
+    }
+    if(cursors.left.isDown || cursors.right.isDown){
+      movingV = 1;
+    }
+    switch(true){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed * movingH;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed * movingH;
+        break;
+    }
+    switch(true){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed * movingV;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed * movingV;
+        break;
+    }
+  };
+
+  function handlePlayerFire(){
+    console.log('fire');
+  }
+
+
 
   function preload() {
     game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
@@ -16,28 +50,14 @@
 
   function create() {
     cursors = game.input.keyboard.createCursorKeys();
+    cursors.fire = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    cursors.fire.onUp.add(handlePlayerFire);
     player = game.add.sprite(100, 100, GFX, 8);
     player.moveSpeed = INITIAL_MOVESPEED;
+    playerBullets = game.add.group();
   };
 
-  function handlePlayerMovement(){
-    switch(true){
-      case cursors.left.isDown:
-        player.x -= player.moveSpeed;
-        break;
-      case cursors.right.isDown:
-        player.x += player.moveSpeed;
-        break;
-    }
-    switch(true){
-      case cursors.down.isDown:
-        player.y += player.moveSpeed;
-        break;
-      case cursors.up.isDown:
-        player.y -= player.moveSpeed;
-        break;
-    }
-  }
+
 
   function update() {
     handlePlayerMovement();
